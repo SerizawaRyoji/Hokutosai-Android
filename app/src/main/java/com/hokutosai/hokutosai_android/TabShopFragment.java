ebd73d387872a1a19787581a603200861df51418
SerizawaRyoji <i1111418.ryoji@gmail.com>
2016-05-02 11:53:07 +0900

    [add]listviewのテスト

--- app/src/main/java/com/hokutosai/hokutosai_android/TabShopFragment.java
+++ app/src/main/java/com/hokutosai/hokutosai_android/TabShopFragment.java
@@ -1,10 +1,14 @@
 package com.hokutosai.hokutosai_android;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by ryoji on 2016/05/01.
@@ -12,16 +16,25 @@ import android.view.ViewGroup;
 public class TabShopFragment  extends Fragment {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // TODO 自動生成されたメソッド・スタブ
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO 自動生成されたメソッド・スタブ
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.tab_shop_fragment, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+
+        ListView listView = (ListView)getActivity().findViewById(R.id.list_shop_view);
+
+        ArrayList<ShopItem> list = new ArrayList<>();
+        ShopItem s = new ShopItem(); s.setName("test"); s.setTenant("tenant");
+        list.add(s);
+
+        ShopItemAdapter adapter = new ShopItemAdapter( getActivity() );
+        adapter.setShopItemList(list);
+        listView.setAdapter(adapter);
+    }
 }
