4c2b1ad989b373825ce28352d7f94dd09fa9ce33
SerizawaRyoji <i1111418.ryoji@gmail.com>
2016-05-05 00:49:02 +0900

    [add] divide new layout

---
+++ app/src/main/java/com/hokutosai/hokutosai_android/NewsFragment.java
@@ -0,0 +1,30 @@
+package com.hokutosai.hokutosai_android;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ryoji on 2016/05/05.
+ */
+public class NewsFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
--- app/src/main/java/com/hokutosai/hokutosai_android/TabNewsFragment.java
+++ app/src/main/java/com/hokutosai/hokutosai_android/TabNewsFragment.java
@@ -2,6 +2,8 @@ package com.hokutosai.hokutosai_android;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,8 +22,22 @@ public class TabNewsFragment  extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // TODO 自動生成されたメソッド・スタブ
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        //Fragment内でFragmentを作成する場合は動的にFragmentを作成しなければならない
+        //レイアウトに使うフラグメントを作成
+        //EventTopicFragment topicFragment = new EventTopicFragment();
+        NewsFragment newsFragment = new NewsFragment();
+
+        //Fragmentを管理するFragmentManagerを取得
+        FragmentManager manager = getChildFragmentManager();
+        //追加や削除などを1つの処理としてまとめるためのトランザクションクラスを取得
+        FragmentTransaction tx = manager.beginTransaction();
+
+        //tx.replace(R.id.layout_fragment_topic_news, topicFragment);
+        tx.replace(R.id.layout_fragment_list_news, newsFragment);
+        tx.commit();
+
+        return inflater.inflate(R.layout.tab_news_fragment, container, false);
     }
 
 }
