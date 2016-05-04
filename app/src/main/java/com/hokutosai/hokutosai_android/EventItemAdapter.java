6282b60bed54fcd1d49d278c995383886338f97d
SerizawaRyoji <i1111418.ryoji@gmail.com>
2016-05-04 16:58:22 +0900

    [add]イベントのリストビューの追加

---
+++ app/src/main/java/com/hokutosai/hokutosai_android/EventItemAdapter.java
@@ -0,0 +1,63 @@
+package com.hokutosai.hokutosai_android;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+import java.util.List;
+
+/**
+ * Created by ryoji on 2016/05/04.
+ */
+public class EventItemAdapter extends ArrayAdapter<EventItem> {
+
+    private LayoutInflater mInflater;
+    private int resource = 0;
+
+    public EventItemAdapter(Context context, List<EventItem> data, int resource) {
+        super(context, 0, data);
+        mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.resource = resource;	//リソースを保持しておく
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        final ViewHolder holder;
+        if (convertView == null) {	//取得すべきビューがnullなら
+            convertView = mInflater.inflate(this.resource, parent, false);
+            holder = new ViewHolder();
+            holder.title = (TextView)convertView.findViewById(R.id.event_item_title);
+            holder.date = (TextView)convertView.findViewById(R.id.event_item_date);
+            holder.state = (TextView)convertView.findViewById(R.id.event_item_state);
+            holder.image = (NetworkImageView)convertView.findViewById(R.id.event_item_image);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+        final EventItem item = getItem(position);
+        holder.title.setText(item.getTitle());
+        holder.date.setText(item.getDate());
+        holder.state.setText("test");
+
+        if(item.getImage_url() != null){
+            holder.image.setImageUrl(item.getImage_url(), ImageLoaderSingleton.getImageLoader(RequestQueueSingleton.getInstance(), LruCacheSingleton.getInstance()) );
+            //holder.image.setTag(mImageLoader.get(item.getImageResource(), listener));
+        }
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        TextView title;
+        TextView date;
+        TextView state;
+        NetworkImageView image;
+    }
+}
--- app/src/main/java/com/hokutosai/hokutosai_android/EventListFragment.java
+++ app/src/main/java/com/hokutosai/hokutosai_android/EventListFragment.java
@@ -1,9 +1,60 @@
 package com.hokutosai.hokutosai_android;
 
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 /**
  * Created by ryoji on 2016/05/04.
  */
 public class EventListFragment extends ListFragment {
+
+    private List<EventItem> list;
+    private ArrayAdapter<EventItem> adapter;
+
+    public void setEventListFragment( List<EventItem> list){
+        //list = new ArrayList<EventItem>();
+        this.list = list;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        adapter=null;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+
+        if(list.isEmpty()) Log.d("test","list is empty");
+
+        if(adapter == null && !list.isEmpty()) {
+            adapter = new EventItemAdapter(getActivity(), list, R.layout.event_item);
+            setListAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO 自動生成されたメソッド・スタブ
+        super.onListItemClick(l, v, position, id);
+
+       //Intent i = new Intent(getActivity(), ShopShowActivity.class);
+        //i.putExtra("ListShopItem", list.get(position));
+        //startActivity(i);
+    }
+
+    @Override
+    public void onStop() {      //ここでwepAPIの読み込みを注視する文を書きたい
+        // TODO 自動生成されたメソッド・スタブ
+        super.onStop();
+    }
 }
