3c1ffffb26c6e771874d3fa35b28a25785ff54c6
SerizawaRyoji <i1111418.ryoji@gmail.com>
2016-05-04 23:58:16 +0900

    [change]イベントのトピックに画像が表示されるよう修正

--- app/src/main/java/com/hokutosai/hokutosai_android/EventTopicFragment.java
+++ app/src/main/java/com/hokutosai/hokutosai_android/EventTopicFragment.java
@@ -3,19 +3,43 @@ package com.hokutosai.hokutosai_android;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.NetworkImageView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.uphyca.android.loopviewpager.LoopViewPager;
+
+import org.json.JSONArray;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by ryoji on 2016/05/04.
  */
 public class EventTopicFragment extends Fragment {
 
-    private final static int[] COLOR_LIST = {0xff7cd5aa, 0xfff1e6a2, 0xfffecc5a, 0xffff8b58, 0xffe92440};
+    ArrayList<EventTopicItem> list;
+    MyViewPagerAdapter adapter;
+
+    //Volleyでリクエスト時に設定するタグ名。キャンセル時に利用する。
+    private static final Object TAG_EVENT_TOPIC_REQUEST_QUEUE = new Object();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        list = new ArrayList<>();
+        adapter = new MyViewPagerAdapter();
+    }
 
     @Nullable
     @Override
@@ -28,16 +52,45 @@ public class EventTopicFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        final LoopViewPager viewPager = (LoopViewPager) getActivity().findViewById(R.id.event_topic_view_pager);
-        //final LxIndicatorGroup indicatorGroup = (LxIndicatorGroup) findViewById(R.id.indicator_view);
-        //indicatorGroup.setup(VIEW_COUNT, R.drawable.indicator);
-        MyViewPagerAdapter adapter = new MyViewPagerAdapter();
-        for (int color : COLOR_LIST) {
-            FrameLayout view = new FrameLayout(getActivity());
-            view.setBackgroundColor(color);
-            adapter.addView(view);
+        if(list.isEmpty()) {
+            MyJsonArrayRequest jArrayRequest =
+                    new MyJsonArrayRequest("https://api.hokutosai.tech/2016/events/topics",
+                            new Response.Listener<JSONArray>() {
+                                @Override
+                                public void onResponse(JSONArray response) {
+
+                                    //JSONArrayをListShopItemに変換して取得
+                                    Gson gson = new Gson();
+                                    Type collectionType = new TypeToken<Collection<EventTopicItem>>() {
+                                    }.getType();
+                                    list = gson.fromJson(response.toString(), collectionType);
+
+                                    //UIに反映
+                                    final LoopViewPager viewPager = (LoopViewPager) getActivity().findViewById(R.id.event_topic_view_pager);
+
+                                    for(int i=0 ; i<list.size() ; ++i){
+                                        NetworkImageView view = new NetworkImageView(getActivity());
+                                        view.setImageUrl(list.get(i).getImage_url(), ImageLoaderSingleton.getImageLoader(RequestQueueSingleton.getInstance(), LruCacheSingleton.getInstance()) );
+
+                                        view.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                                        adapter.addView(view);
+                                    }
+                                    viewPager.setAdapter(adapter);
+                                }
+                            },
+
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.e("LIFE", error.toString());
+                                    // エラー処理 error.networkResponseで確認
+                                    // エラー表示など
+                                }
+                            });
+
+            jArrayRequest.setCustomTimeOut();   //タイムアウト時間の変更
+            jArrayRequest.setTag(TAG_EVENT_TOPIC_REQUEST_QUEUE);    //タグのセット
+            RequestQueueSingleton.getInstance().add(jArrayRequest);    //WebAPIの呼び出し
         }
-        viewPager.setAdapter(adapter);
-        //viewPager.addOnPageChangeListener(indicatorGroup.getPositionSyncListener());
     }
 }
---
+++ app/src/main/java/com/hokutosai/hokutosai_android/EventTopicItem.java
@@ -0,0 +1,35 @@
+package com.hokutosai.hokutosai_android;
+
+/**
+ * Created by ryoji on 2016/05/04.
+ */
+public class EventTopicItem {
+
+    String title;
+    String image_url;
+    int event_id;
+
+    public int getEvent_id() {
+        return event_id;
+    }
+
+    public void setEvent_id(int event_id) {
+        this.event_id = event_id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
