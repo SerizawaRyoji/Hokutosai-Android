f19453f319cdaef6af2b3d0e20ddf87b65740091
SerizawaRyoji <i1111418.ryoji@gmail.com>
2016-05-05 15:28:17 +0900

    [add]詳細ビューの追加

--- app/src/main/java/com/hokutosai/hokutosai_android/ExhibitionDetailActivity.java
+++ app/src/main/java/com/hokutosai/hokutosai_android/ExhibitionDetailActivity.java
@@ -3,7 +3,18 @@ package com.hokutosai.hokutosai_android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.NetworkImageView;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -12,11 +23,15 @@ import java.util.List;
  */
 public class ExhibitionDetailActivity extends Activity {
 
+    private ExhibitionDetail mExhibitionDetail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_exhibition_detail);
+
+        mExhibitionDetail = new ExhibitionDetail();
 
         Intent i = getIntent();
         final Exhibition item = (Exhibition)i.getSerializableExtra("Exhibition");
@@ -32,6 +47,55 @@ public class ExhibitionDetailActivity extends Activity {
         String url = "https://api.hokutosai.tech/2016/exhibitions/";
         url += String.valueOf(item.getExhibition_id());
         url += "/details";
+
+        MyJsonObjectRequest jObjectRequest =
+                new MyJsonObjectRequest(Request.Method.GET,url,null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                //JSONArrayをListShopItemに変換して取得
+                                Gson gson = new Gson();
+                                mExhibitionDetail = gson.fromJson(response.toString(), ExhibitionDetail.class);
+
+                                //画像の表示********************************************************************************************
+                                NetworkImageView image = (NetworkImageView) findViewById(R.id.exhibition_detail_image);
+                                int imagewidth = image.getWidth();
+                                image.setLayoutParams(new LinearLayout.LayoutParams(imagewidth, imagewidth));
+                                image.setImageUrl(item.getImage_url(), ImageLoaderSingleton.getImageLoader(RequestQueueSingleton.getInstance(), LruCacheSingleton.getInstance()));
+                                //******************************************************************************************************
+
+
+                                //場所の表示*********************************************************************************************
+                                TextView place = (TextView)findViewById(R.id.exhibition_detail_place);
+                                place.setText(mExhibitionDetail.place.getName());
+                                //*****************************************************************************************************
+
+                                //いいねの表示*******************************************************************************************
+                                ImageView like = (ImageView)findViewById(R.id.exhibition_detail_like);
+                                TextView like_count = (TextView)findViewById(R.id.exhibition_detail_like_count);
+                                like.setImageResource(R.mipmap.like_selected);
+                                like_count.setText("いいね：" + String.valueOf(mExhibitionDetail.likes_count) + "件");
+                                //*****************************************************************************************************
+
+                                //説明文*************************************************************************************************
+                                TextView introduction = (TextView)findViewById(R.id.exhibition_detail_introduction);
+                                introduction.setText(mExhibitionDetail.introduction);
+                                //******************************************************************************************************
+                            }
+                        },
+
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("LIFE", error.toString());
+                                // エラー処理 error.networkResponseで確認
+                                // エラー表示など
+                            }
+                        });
+
+        jObjectRequest.setCustomTimeOut();   //タイムアウト時間の変更
+        RequestQueueSingleton.getInstance().add(jObjectRequest);    //WebAPIの呼び出し
     }
 
     private class ExhibitionDetail{
@@ -42,6 +106,7 @@ public class ExhibitionDetailActivity extends Activity {
         String image_url;
         AssessedScore assessed_score;
         Boolean liked;
+        int likes_count;
         String introduction;
         PlaceItem place;
         List<Assessment> assessments;
