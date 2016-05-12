d9e08923cde4d2f1a2c0f6834e651d76f8191966
SerizawaRyoji <i1111418.ryoji@gmail.com>
2016-05-12 22:43:10 +0900

    [change]詳細ビューの修正

--- app/src/main/java/com/hokutosai/hokutosai_android/ExhibitionDetailActivity.java
+++ app/src/main/java/com/hokutosai/hokutosai_android/ExhibitionDetailActivity.java
@@ -1,13 +1,16 @@
 package com.hokutosai.hokutosai_android;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -25,6 +28,10 @@ import java.util.List;
 public class ExhibitionDetailActivity extends AppCompatActivity {
 
     private ExhibitionDetail mExhibitionDetail;
+    int mLikeCount;             //いいねの件数
+
+    //Volleyでリクエスト時に設定するタグ名。キャンセル時に利用する。
+    private static final Object TAG_EXHIBITION_LIKE_REQUEST_QUEUE = new Object();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,8 @@ public class ExhibitionDetailActivity extends AppCompatActivity {
         Intent i = getIntent();
         final Exhibition item = (Exhibition)i.getSerializableExtra("Exhibition");
 
+        mLikeCount = item.getLike_count();
+
         TextView name = (TextView)this.findViewById(R.id.exhibition_detail_name);
         TextView exhibitors = (TextView)this.findViewById(R.id.exhibition_detail_exhibitors);
         TextView displays = (TextView)this.findViewById(R.id.exhibition_detail_displays);
@@ -47,6 +56,8 @@ public class ExhibitionDetailActivity extends AppCompatActivity {
         name.setText(item.getTitle());
         exhibitors.setText(item.getExhibitors());
         displays.setText(item.getDisplays());
+
+        setLikeClickEvent(item, this);
 
         String url = "https://api.hokutosai.tech/2016/exhibitions/";
         url += String.valueOf(item.getExhibition_id());
@@ -58,7 +69,6 @@ public class ExhibitionDetailActivity extends AppCompatActivity {
                             @Override
                             public void onResponse(JSONObject response) {
 
-                                //JSONArrayをListShopItemに変換して取得
                                 Gson gson = new Gson();
                                 mExhibitionDetail = gson.fromJson(response.toString(), ExhibitionDetail.class);
 
@@ -76,10 +86,14 @@ public class ExhibitionDetailActivity extends AppCompatActivity {
                                 //*****************************************************************************************************
 
                                 //いいねの表示*******************************************************************************************
-                                ImageView like = (ImageView)findViewById(R.id.exhibition_detail_like);
+                                mLikeCount = mExhibitionDetail.likes_count;
                                 TextView like_count = (TextView)findViewById(R.id.exhibition_detail_like_count);
-                                like.setImageResource(R.drawable.like_selected);
-                                like_count.setText("いいね：" + String.valueOf(mExhibitionDetail.likes_count) + "件");
+                                like_count.setText("いいね：" + String.valueOf(mLikeCount) + "件");
+                                //*****************************************************************************************************
+
+                                //いいね(クリック可)の表示********************************************************************************
+                                ImageView like = (ImageView)findViewById(R.id.exhibition_detail_like_clickable);
+                                like.setSelected(mExhibitionDetail.liked);
                                 //*****************************************************************************************************
 
                                 //説明文*************************************************************************************************
@@ -110,6 +124,63 @@ public class ExhibitionDetailActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        RequestQueueSingleton.getInstance().cancelAll(TAG_EXHIBITION_LIKE_REQUEST_QUEUE);
+    }
+
+    private void setLikeClickEvent(final Exhibition item, final Activity activity){
+
+        final ImageView like = (ImageView)findViewById(R.id.exhibition_detail_like_clickable);
+        // ImageViewオブジェクトにクリックイベントを追加する
+        if(like != null) {
+            like.setOnClickListener(
+                    new View.OnClickListener() {
+                        public void onClick(View v) {
+
+                            String url = "https://api.hokutosai.tech/2016/exhibitions/" + item.getExhibition_id() + "/likes";
+
+                            MyStringRequest postJson = new MyStringRequest(Request.Method.POST, url,
+                                    new Response.Listener<String>() {
+                                        @Override
+                                        public void onResponse(String response) {
+                                            // TODO 自動生成されたメソッド・スタブ
+                                            if( activity != null) {
+                                                like.setSelected(!like.isSelected());   //画像の反転
+
+                                                TextView like_count = (TextView)findViewById(R.id.exhibition_detail_like_count);
+
+                                                if( like.isSelected() ){
+                                                    Toast.makeText(activity, "いいねしました", Toast.LENGTH_SHORT).show();
+                                                    ++mLikeCount;
+                                                    like_count.setText("いいね：" + String.valueOf(mLikeCount) + "件");
+                                                }
+                                                else{
+                                                    Toast.makeText(activity, "いいねを取り消しました", Toast.LENGTH_SHORT).show();
+                                                    --mLikeCount;
+                                                    like_count.setText("いいね：" + String.valueOf(mLikeCount) + "件");
+                                                }
+                                            }
+                                        }
+                                    },
+                                    new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+                                            if(activity != null) Toast.makeText(activity, "いいねに失敗しました", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                            );
+
+                            postJson.setCustomTimeOut();   //タイムアウト時間の変更
+                            postJson.setTag(TAG_EXHIBITION_LIKE_REQUEST_QUEUE);    //タグのセット
+                            RequestQueueSingleton.getInstance().add(postJson);    //WebAPIの呼び出し
+                        }
+                    }
+            );
+        }
     }
 
     private class ExhibitionDetail{
