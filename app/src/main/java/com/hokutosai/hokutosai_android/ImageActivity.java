0f4acd35ab73c8d90a681b2daca81687201d48e4
SerizawaRyoji <i1111418.ryoji@gmail.com>
2016-05-18 23:18:45 +0900

    [add]画像の保存機能の追加

--- app/src/main/java/com/hokutosai/hokutosai_android/ImageActivity.java
+++ app/src/main/java/com/hokutosai/hokutosai_android/ImageActivity.java
@@ -1,11 +1,21 @@
 package com.hokutosai.hokutosai_android;
 
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
@@ -16,6 +26,7 @@ public class ImageActivity extends AppCompatActivity {
 
     ImageView mImageView;
     PhotoViewAttacher mAttacher;
+    Bitmap bmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +37,7 @@ public class ImageActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_image);
 
-        Bitmap bmp = MyApplication.getInstance().getBmp();
+        bmp = MyApplication.getInstance().getBmp();
 
         mImageView = (ImageView)findViewById(R.id.image_activity);
         mImageView.setImageBitmap(bmp);
@@ -48,7 +59,60 @@ public class ImageActivity extends AppCompatActivity {
             case android.R.id.home:   // アプリアイコン（ホームアイコン）を押した時の処理
                 finish();
                 break;
+
             case R.id.menu_download:    //ダウンロードボタン
+
+                try {
+                    // sdcardフォルダを指定
+                    File file = new File(Environment.getExternalStorageDirectory() + "/Hokutosai-Pictures/");
+                    Log.d("test",file.getPath());
+                    try{
+                        if(!file.exists()){
+                            file.mkdir();
+                        }
+                    }catch(SecurityException e){
+                        e.printStackTrace();
+                        throw e;
+                    }
+
+                    //日付でファイル名を作成
+                    Date mDate = new Date();
+                    SimpleDateFormat fileNameDate = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                    String fileName = fileNameDate.format(mDate) + ".jpg";
+                    String AttachName = file.getAbsolutePath() + "/" + fileName;
+
+                    try {
+                        FileOutputStream out = new FileOutputStream(AttachName);
+                        bmp.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                        out.flush();
+                        out.close();
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                        throw e;
+                    }
+
+                    //最後のContentResolverの部分で保存したファイルパスを登録   *これをしないとデバイスを再起動するまでギャラリーに表示されない
+                    /*ContentValues values = new ContentValues();
+                    ContentResolver contentResolver = getContentResolver();
+                    values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                    values.put(MediaStore.Images.Media.TITLE, fileName);
+                    values.put("_data", AttachName);
+                    contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);*/
+
+                    File f = new File(Environment.getExternalStorageDirectory() + "/Hokutosai-Pictures/",fileName);
+
+                    // MediaScannerConnection を使用する場合
+                    MediaScannerConnection.scanFile( // API Level 8
+                            this, // Context
+                            new String[] { f.getPath() },
+                            new String[] { "image/jpeg" },
+                            null);
+
+                    Toast.makeText(ImageActivity.this,file.getPath() + "に画像を保存しました",Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Log.e("Error", "" + e.toString());
+                    Toast.makeText(ImageActivity.this,"画像の保存に失敗しました",Toast.LENGTH_SHORT).show();
+                }
 
                 break;
         }
